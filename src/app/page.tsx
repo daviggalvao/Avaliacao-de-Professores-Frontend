dbdef29778b89e20f7dd51f9088eb9660f4bb650
gusfring41 <gusfring.a@gmail.com>
2024-11-23 23:20:08 -0300

    feat:button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,15 @@
 'use client';
 
 import Image from "next/image";
+import './globals.css';
+;
+
 
 export default function Home() {
   return (
-
-    <div>
-      <style jsx>{`
-      
-        .img {
-          height: 100%;
-          width: 50%;
-          object-fit: cover;
-        }
-
-        .form {
-          position: fixed;
-          top: 200px; 
-          right: 150px; 
-          background-color: #f0f0f0;
-          padding: 20px;
-          border-radius: 5px;
-        }
-      
-        .h {
-          position: fixed;
-          top: 10px; 
-          right:160px; 
-          padding: 20px;
-        }
-      `}</style>
-
-      <Image src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt = "Grupo de estudantes em uma roda"
-      className = "img" width={500} height ={500}/>
+  <div>
+    <Image src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      alt = "Grupo de estudantes em uma roda" className = "img" width={500} height ={500}/>
 
       <h1 className = "h">Avaliação de professores</h1>
 
@@ -42,7 +19,12 @@ export default function Home() {
           <label htmlFor="senha">Senha:</label><br/>
           <input type="password" id="senha" name="senha" placeholder = "Digite aqui sua senha..."/>
       </form> 
-    </div>
+
+      <div className="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
+        <button className = "button">Entrar </button>
+        <button className = "button2">Criar Conta</button>
+      </div>
+  </div>
 
   );
 }
