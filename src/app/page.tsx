4a6cedab26a89b32a1bf2698cf1b84c7d917e342
gusfring41 <gusfring.a@gmail.com>
2024-11-24 11:24:36 -0300

    fix: new button and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     <Image src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
       alt = "Grupo de estudantes em uma roda" className = "img" width={500} height ={500}/>
 
-      <h1 className = "h">Avaliação de professores</h1>
+      <h1 className = "h1">Avaliação de professores</h1>
 
       <form className = "form">
-          <label htmlFor="email">Email:</label><br/>
-          <input type="email" id="email" name="email" placeholder = "Digite aqui seu email..."/><br/>
-          <label htmlFor="senha">Senha:</label><br/>
-          <input type="password" id="senha" name="senha" placeholder = "Digite aqui sua senha..."/>
+          <label htmlFor="email"></label><br/>
+          <input className = "button3" type="email" id="email" name="email" placeholder = "Email"/><br/>
+          <label htmlFor="senha"></label><br/>
+          <input className = "button4" type="password" id="senha" name="senha" placeholder = "Senha"/>
       </form> 
 
       <div className="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
