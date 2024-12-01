63b0b44745bdf9beeb3cb81d93669254dda5854e
gusfring41 <gusfring.a@gmail.com>
2024-12-01 11:29:40 -0300

    refact: module files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,33 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import './styles/globals.css';
+import styles from './styles/login.module.css';
+
 
 export default function Home() {
   return (
-  <div className = "janela">
+  <div className = {styles.janela}>
     <Image src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-      alt = "Grupo de estudantes em uma roda" className = "img metadeesq" width={500} height ={500}/>
+      alt = "Grupo de estudantes em uma roda" className = {styles.metadeesq} width={500} height ={500}/>
 
-      <div className = "metadedir">
+      <div className = {styles.metadedir}>
         <br/>
         <br/>
-        <h1 className = "AvProf">Avaliação de Professores</h1>
+        <h1 className= {styles.title}>Avaliação de Professores</h1>
         <form>
           <br/>
           <br/>
             <label htmlFor="email"></label><br/>
-            <input className = "buttonf log" type="email" id="email" name="email" placeholder = "Email"/>
+            <input className = {styles.buttonf} type="email" id="email" name="email" placeholder = "Email"/>
             <label htmlFor="senha"></label><br/>
-            <input className = "buttonf log" type="password" id="senha" name="senha" placeholder = "Senha"/>
+            <input className = {styles.buttonf} type="password" id="senha" name="senha" placeholder = "Senha"/>
         </form> 
 
         <div className="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
-          <button className = "buttonc createlog">Entrar </button>
+          <button className = {styles.buttonc} >Entrar </button>
           <Link href = "/register">
-            <button className = "buttonc createlog">Criar Conta</button>
+            <button className = {styles.buttonc} >Criar Conta</button>
           </Link>
           
         </div>
--- src/app/regpfp/page.tsx
+++ src/app/regpfp/page.tsx
@@ -1,38 +1,41 @@
 'use client';
 
 import Image from "next/image";
+import '../styles/globals.css';
+import styles from '../styles/login.module.css';
+import stylespfp from '../styles/regpfp.module.css';
 
 export default function Home() {
   return (
-  <div className = "janela">
+  <div className = {styles.janela}>
     <Image src="https://plus.unsplash.com/premium_photo-1691962725045-57ff9e77f0bd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-      alt = "Grupo de estudantes em uma roda" className = "img metadeesq" width={500} height ={500}/>
+      alt = "Grupo de estudantes em uma roda" className = {styles.metadeesq} width={500} height ={500}/>
 
-      <div className = "metadedir">
+      <div className = {styles.metadedir}>
 
       <Image src="https://th.bing.com/th/id/OIP.hcRhDT8KVqzySjYJmBhlzgHaHa?rs=1&pid=ImgDetMain" 
-      alt = "Foto de perfil não cadastrada" className = "pfp" width={100} height ={100}/>
+      alt = "Foto de perfil não cadastrada" className = {stylespfp.pfp} width={100} height ={100}/>
           
           <form>
               <label htmlFor="email"></label><br/>
-              <input className = "buttonf create" type="email" id="email" name="email" placeholder = "Email"/>
+              <input className = {styles.buttonf} type="email" id="email" name="email" placeholder = "Email"/>
 
               <label htmlFor="senha"></label><br/>
-              <input className = "buttonf create" type="password" id="senha" name="senha" placeholder = "Senha"/>
+              <input className = {styles.buttonf} type="password" id="senha" name="senha" placeholder = "Senha"/>
 
               <label htmlFor="nome"></label><br/>
-              <input className = "buttonf create" type="name" id="nome" name="nome" placeholder = "Nome"/>
+              <input className = {styles.buttonf} type="name" id="nome" name="nome" placeholder = "Nome"/>
 
               <label htmlFor="curso"></label><br/>
-              <input className = "buttonf create" type="name" id="curso" name="curso" placeholder = "Curso"/>
+              <input className = {styles.buttonf} type="name" id="curso" name="curso" placeholder = "Curso"/>
 
               <label htmlFor="departamento"></label><br/>
-              <input className = "buttonf create" type="name" id="departamento" name="departamento" placeholder = "Departamento"/>
+              <input className = {styles.buttonf} type="name" id="departamento" name="departamento" placeholder = "Departamento"/>
 
           </form> 
 
           <div className="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
-            <button className = "buttonc criarlogreg">Criar Conta</button>
+            <button className = {styles.buttonc}>Criar Conta</button>
           </div>
         </div>
   </div>
