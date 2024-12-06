d88841ee899b0c6da531f1924849e0c1b41d4dd9
Galvão <daviguerra27@gmail.com>
2024-12-06 07:09:41 -0300

    fix box coment

--- src/app/modalcomentedit/page.tsx
+++ src/app/modalcomentedit/page.tsx
@@ -13,8 +13,11 @@ export default function Home() {
             <div className={`h-full w-full bg-black/40 fixed top-0 left-0 transition-all ${open? "opacity-100" : "opacity-0"}`}>
               <br />
                 <div className={stylesmc.caixam}>
-                  <div className="flex-1 outline-none w-full text-start flex text-wrap rounded-3xl">
+                  <div className="bg-foreground flex flex-col items-center justify-center rounded-md h-4/5 w-11/12">
+                    <div className="w-full h-[2px] bg-background mt-10"></div>
+                    <div className="flex-1 outline-none w-full text-start flex text-wrap rounded-3xl">
                       <textarea className="outline-none bg-foreground resize-none w-full" id="comment"/>
+                    </div>
                   </div>
                   <div className={stylesmc.space}>
                     <button className = {stylesmc.buttonc}>Comentar</button>
