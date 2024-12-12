6de000e31ffd837e42e13e313da47e793485a6fd
Galvão <daviguerra27@gmail.com>
2024-12-12 17:06:01 -0300

    fix modal perfil | add botao de editar perfil

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -6,35 +6,42 @@ import "../styles/globals.css";
 import HeaderDeslogado from "@/components/HeaderDeslogado";
 import HeaderLogado from "@/components/HeaderLogado";
 import Coment from "@/components/Coment";
+import ModalPerfil from "@/components/ModalPerfil";
 
 export default function Home() {
   return (
     <div>
       <HeaderDeslogado />
 
-      <div className="flex flex-col justify-center items-center min-h-screen bg-white">
-        <Link href="/">
-          <button className="absolute bg-foreground p-1 mt-4 ml-52 flex justify-center items-center w-28 rounded-xl">Sair</button>
-        </Link>
-
+      <div className="flex justify-center items-center min-h-screen bg-white">
         <div className="bg-white w-1/2 h-full border-2 border-foreground ">
-          <div className="bg-white-100 w-full h-1/4 border-b-2 border-foreground">
-            <Image
-              src="perfil.svg"
-              alt="Foto de Perfil"
-              width={25}
-              height={25}
-              className="w-32 h-32 border border-background rounded-full ml-16 mt-8"
-            />
+          <div className="bg-white flex w-full h-1/4 border-b-2 border-foreground">
+            <div className="flex basis-2/4 flex-col">
+              <Image
+                src="perfil.svg"
+                alt="Foto de Perfil"
+                width={25}
+                height={25}
+                className="w-32 h-32 border border-background rounded-full ml-16 mt-8"
+              />
 
-            <h1 className="text-xl font-bold mt-2 ml-16">Nome do aluno</h1>
+              <h1 className="text-xl font-bold mt-2 ml-16">Nome do aluno</h1>
 
-            <h2 className="text-xs ml-16">Ciência da Computação</h2>
+              <h2 className="text-xs ml-16">Ciência da Computação</h2>
 
-            <h2 className="text-xs ml-16">aluno123@gmail.com</h2>
-            <br />
+              <h2 className="text-xs ml-16">aluno123@gmail.com</h2>
+              <br />
+            </div>
+
+            <div className="flex basis-3/4 flex-col justify-start items-end mr-4">
+              <ModalPerfil></ModalPerfil>
+              <button className="bg-foreground p-1 mt-4 flex justify-center items-center w-28 rounded-xl">Excluir</button>
+              <Link href="/">
+                <button className="bg-foreground p-1 mt-4 flex justify-center items-center w-28 rounded-xl">Sair</button>
+              </Link>
+            </div>
           </div>
-
+          
           <div className="flex flex-col items-center justify-center">
             <h1 className="text-xl font-bold">Publicações</h1>
 
@@ -47,3 +54,5 @@ export default function Home() {
     </div>
   );
 }
+
+
---
+++ src/components/ModalPerfil.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+
+const ModalPerfil = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+        <button onClick={() => setOpen(!open)} className="bg-foreground p-1 mt-4 flex justify-center items-center w-28 rounded-xl">Editar</button>
+        {open ? (
+          <div className={"h-full w-full bg-black/40 fixed top-0 left-0 flex flex-col justify-center items-center"}>
+            <div className="bg-background rounded-xl flex flex-col justify-center items-center relative w-1/3">
+            
+                <div onClick={() => setOpen(false)} className="text-[#aaa] absolute right-3 cursor-pointer top-3 text-3xl font-bold">&times;</div>
+
+                <Image src="perfil.svg" 
+                alt = "Foto de perfil não cadastrada" className="mt-4 rounded-full cursor-pointer" width={100} height ={100}/>
+
+                <form className = "text-black w-4/5">
+                  <label htmlFor="nome"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="name" id="nome" name="nome" placeholder = "Nome"/>
+
+                  <label htmlFor="email"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="email" id="email" name="email" placeholder = "Email"/>
+
+                  <label htmlFor="curso"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="name" id="curso" name="curso" placeholder = "Curso"/>
+
+                  <label htmlFor="departamento"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="name" id="departamento" name="departamento" placeholder = "Departamento"/>
+
+                  <label htmlFor="senha"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="password" id="senha" name="senha" placeholder = "Senha atual"/>
+
+                  <label htmlFor="senha"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="password" id="senha" name="senha" placeholder = "Nova senha"/>
+
+                  <label htmlFor="senha"></label><br/>
+                  <input className = "text-center relative w-full mb-3 p-3 rounded-3xl" type="password" id="senha" name="senha" placeholder = "Confirmar nova senha"/>
+                </form> 
+                
+                <button onClick={() => setOpen(false)} className="text-center bg-foreground mb-4 w-28 text-white p-3 rounded-xl">Salvar</button>
+            </div>
+        </div>
+    ) : (
+      <div className="hidden"></div>
+    )}
+      </>
+  );
+};
+
+export default ModalPerfil;
