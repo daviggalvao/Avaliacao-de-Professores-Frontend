752c58721c808cc8b394d607db5e69ef0011cca8
Lucas Teixeira <lucastsciencia@gmail.com>
2024-12-19 16:37:57 -0300

    últimas atualizações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { useEffect } from "react";
-import { UserData } from "../../types/User";
+import { UserData } from "@/types/User";
 import { getToken } from "../utils/auth";
 import Link from "next/link";
 import styles from '../aa_extra/styles/login.module.css';
@@ -11,91 +11,77 @@ import styles from '../aa_extra/styles/login.module.css';
 import Professor from '../components/entidades/Professor';
 import { hookAllProfessores } from '../hooks/hookProfessores'; 
 import { hookAllUsers } from '../hooks/hookUser'; 
-
+import { IsAuthenticated, getStorageUser } from "../utils/auth";
 import Header from '../components/layout/Header';
 import ModalAvaliação from "@/components/modais/ModalAvaliação";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [User, setUser] = useState<UserData | null>(null); // Dados do usuário autenticado
+  const [input, setInput] = useState("");
   const professores = hookAllProfessores();
   const usuarios = hookAllUsers();
-
-  useEffect(() => {
-    const token = getToken(); // Verifica se existe um token
-    if (token) {
-      setIsAuthenticated(true);
-
-      // Busca os dados do usuário autenticado do localStorage
-      const userData = localStorage.getItem("user");
-      if (userData) {
-        setUser(JSON.parse(userData));
-      }
-    }
-  }, []);
 
   //const sixMonthsAgo = new Date();
   //sixMonthsAgo.setDay(sixMonthsAgo.getMonth() - 6);
 
-  const today = new Date();
 
   // Criar uma nova data para 2 dias atrás
+  const today = new Date();
   const twoDaysAgo = new Date(today);
   twoDaysAgo.setDate(today.getDate() - 2);
 
   console.log(twoDaysAgo);
 
   return (
-  <div>
-    <Header/>
-    
-    <div className="bg-sky-100 h-screen w-screen">
-        <div className="bg-sky-100 ml-4 flex justify-between items-center">
-          {/* component modal de avaliacao no lugar do button | deslogado->login || logado -> modal avaliacao*/}
-          <div className="mt-4 flex">
-            {isAuthenticated ? (
-                <ModalAvaliação></ModalAvaliação>
-              ) : (
-                <Link href="/auth/login">
-                  <button className = "bg-foreground p-2 mr-3 text-white font-bold flex justify-center items-center rounded-xl">Nova Publicação</button>
-                </Link>
-              )}
+    <div>
+      <Header/>
+      
+      <div className="bg-sky-100 h-screen w-screen">
+          <div className="bg-sky-100 ml-4 flex justify-between items-center">
+            {/* component modal de avaliacao no lugar do button | deslogado->login || logado -> modal avaliacao*/}
+            <div className="mt-4 flex">
+              {IsAuthenticated() ? (
+                  <ModalAvaliação></ModalAvaliação>
+                ) : (
+                  <Link href="/auth/login">
+                    <button className = "bg-foreground p-2 mr-3 text-white font-bold flex justify-center items-center rounded-xl">Nova Publicação</button>
+                  </Link>
+                )}
+            </div>
+            
           </div>
-          
-        </div>
-      <div className="NovosProfessores">
-        <h1 className="ml-4 text-black font-bold my-8">Novos Professores</h1>
+        <div className="NovosProfessores">
+          <h1 className="ml-4 text-black font-bold my-8">Novos Professores</h1>
 
-        <div className="bg-sky-100 grid grid-cols-4 ml-4">
-          {professores.length > 0 ? (
-            professores.filter((professor) => { 
-                return new Date(professor.createdAt) >= twoDaysAgo;
-              }
-            ).map((professor) => (
-              <Professor key={professor.id} {...professor} />
+          <div className="bg-sky-100 grid grid-cols-4 ml-4">
+            {professores.length > 0 ? (
+              professores.filter((professor) => { 
+                  return new Date(professor.createdAt) >= twoDaysAgo;
+                }
+              ).map((professor) => (
+                <Professor key={professor.id} {...professor} />
+                )
               )
-            )
-          ) : (
-            <p className="ml-4 text-black">Nenhum professor encontrado.</p>
-          )}
+            ) : (
+              <p className="ml-4 text-black">Nenhum professor encontrado.</p>
+            )}
+          </div>
         </div>
-      </div>
 
-      <div className="TodosProfessores bg-sky-100">
-        <h1 className = "ml-4 text-black font-bold my-8">Todos os Professores</h1>
+        <div className="TodosProfessores bg-sky-100">
+          <h1 className = "ml-4 text-black font-bold my-8">Todos os Professores</h1>
 
-        <div className="bg-sky-100 grid grid-cols-4 ml-4">
-          {professores.length > 0 ? (
-            professores.map((professor) => (
-              <Professor key={professor.id} {...professor} />
-            ))
-          ) : (
-            <p className="ml-4 text-black">Nenhum professor encontrado.</p>
-          )}
+          <div className="bg-sky-100 grid grid-cols-4 ml-4">
+            {professores.length > 0 ? (
+              professores.map((professor) => (
+                <Professor key={professor.id} {...professor} />
+              ))
+            ) : (
+              <p className="ml-4 text-black">Nenhum professor encontrado.</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
